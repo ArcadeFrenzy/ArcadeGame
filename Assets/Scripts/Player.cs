003d62e0d727b85383bda6292f5d50f54c1dd51c
FilUnderscore <fil@filunderscore.com>
2024-05-03 10:29:31 +1200

    Add player positioning.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,7 +1,9 @@
+using Unity.Netcode;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Player : NetworkBehaviour
 {
+    public NetworkVariable<Vector3> Position = new NetworkVariable<Vector3>();
     public string Username;
 
     // Start is called before the first frame update
@@ -13,6 +15,22 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = Position.Value;
+    }
+
+    [Rpc(SendTo.Server)]
+    void SubmitPositionRequestServerRpc(RpcParams rpcParams = default)
+    {
+        var randomPosition = new Vector3(Random.Range(-3f, 3f), 1f, Random.Range(-3f, 3f));
+        transform.position = randomPosition;
+        Position.Value = randomPosition;
+    }
+
+    public override void OnNetworkSpawn()
+    {
+        if(IsOwner)
+        {
+            SubmitPositionRequestServerRpc();
+        }
     }
 }
